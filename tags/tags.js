053fadf37ea83f5157b89238153ee61ed5203290
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2024-01-25 11:20:18 -0500

    Create tags.js

---
+++ tags/tags.js
@@ -0,0 +1,32 @@
+import "./rpt.js"
+import "./space.js"
+import "./mline.js"
+import "./counter.js"
+import "./searchbar.js"
+
+
+let mline = document.getElementsByTagName("mr")
+for (let i = 0; i < mline.length; i++) {
+  let y = mline[i].getAttribute("num")
+  Hml(y)
+}
+
+
+let sp = document.getElementsByTagName("sp")
+for (let i = 0; i < sp.length; i++) {
+  let snum = sp[i].getAttribute("num")
+  Hsp(snum)
+}
+
+let rpt = document.getElementsByTagName("rpt")
+for (let i = 0; i < rpt.length; i++) {
+  let nm = rpt[i].getAttribute("num")
+  let txt = rpt[i].innerHTML
+  Hrpt(nm, txt)
+}
+
+let cntr = document.getElementsByTagName("counter");
+for (let i = 0 ; i < cntr.length ; i++){
+    let ce = cntr[i].getAttribute("elmt")
+    Hcntr(ce)
+}
